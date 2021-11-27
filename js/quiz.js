e458f2618740c22a85d312932ed1c0924045bfb5
GeyerJoachim <Geyer.Joachim@googlemail.com>
2021-11-27 18:38:40 +0100

    Update and rename quiz.json to quiz.js

---
+++ js/quiz.js
@@ -0,0 +1,175 @@
+var quiz = {
+  "title": "Geburtstag",
+  "img": "imgs/HBsmall.png",
+  "imgFinal": "imgs/minionsFeier.png",
+  "ImgsTF": ['imgs/thumb-up.png','imgs/thumb-down.png'],
+  "Fragen":
+  [
+    {
+      "Frage": "Wann errang Pierre de Coubertin, der Begründer der Olympischen Spiele der Neuzeit, die Goldmedaille in der Disziplin \"Literatur\"?",
+      "Kategorie": "Sport",
+      "Antworten": [
+        "1896",
+        "1912",
+        "1920"
+      ],
+      "Letter": "!",
+      "Loesung": 1
+    },
+    {
+      "Frage": "Wie oft konnte sich Halles Marathonlegende Waldemar Cierpinski Olympisches Gold erlaufen?",
+      "Kategorie": "Sport",
+      "Antworten": [
+        "1-mal",
+        "2-mal",
+        "3-mal"
+      ],
+      "Letter": "y",
+      "Loesung": 1
+    },
+    {
+      "Frage": "Wer spielt heute gegen den amtierenden Titelträger Magnus Carlsen um den Titel des Schachweltmeisters?",
+      "Kategorie": "Schach",
+      "Antworten": [
+        "Anish Giri",
+        "Rustam Kasimdzhanov",
+        "Ian Nepomniachtchi"
+      ],
+      "Letter": "a",
+      "Loesung": 2
+    },
+    {
+      "Frage": "Wann war die erste Frau im All?",
+      "Kategorie": "Weltraum",
+      "Antworten": [
+        "1963",
+        "1981",
+        "1993"
+      ],
+      "Letter": "d",
+      "Loesung": 0
+    },
+    {
+      "Frage": "Wieviel \"RAM\" hatte der Apollo Guidance Computer für die erste Mondlandung?",
+      "Kategorie": "Weltraum",
+      "Antworten": [
+        "4000 Byte",
+        "128 kB",
+        "512 kB"
+      ],
+      "Letter": "h",
+      "Loesung": 0
+    },
+    {
+      "Frage": "Wie heißt die polnische Nationalhymne?",
+      "Kategorie": "Länder",
+      "Antworten": [
+        "Nad Tatrou sa blýska",
+        "Mazurek Dąbrowskiego",
+        "Kde domov můj"
+      ],
+      "Letter": "t",
+      "Loesung": 1
+    },
+    {
+      "Frage": "Wann wurde der SC Westfalia Herne Westdeutscher Fußballmeister?",
+      "Kategorie": "Fußball",
+      "Antworten": [
+	"1940",
+	"1959",
+	"1966"
+      ],
+      "Loesung": 1,
+      "Letter": "r"
+    },
+    {
+      "Frage": "Das Auge im Vorspann des Tatort gehört Horst Lettenmayer. Der verdiente mit seinem Auftritt ...?",
+      "Kategorie": "Tatort",
+      "Antworten": [
+	"nichts",
+	"400 DM",
+	"4000 DM"
+      ],
+      "Loesung": 1,
+      "Letter": "i"
+    },
+
+    {
+      "Frage": "Was nennt man bei Pferden \"Schnippe\"?",
+      "Kategorie": "Pferde",
+      "Antworten": [
+	"ein weißer Fleck zwischen den Nüstern",
+	"eine besonders bockige Stute",
+	"eine Figur beim Dressurreiten"
+      ],
+      "Letter": "B",
+      "Loesung": 0
+    },
+    {
+      "Frage": "Mit einer Ausdehnung von 30.000 Hektar sind die kirgisischen Wälder welcher Art die größten weltweit?",
+      "Kategorie": "Länder",
+      "Antworten": [
+	"Kirsche",
+	"Eiche",
+	"Walnuss"
+      ],
+      "Loesung": 2,
+      "Letter": " "
+    },
+    {
+      "Frage": "Welche Besonderheit weist die koreanische Schrift auf?",
+      "Kategorie": "Länder",
+      "Antworten": [
+        "Sie wurde erst im 15. Jahrhundert künstlich konstruiert.",
+        "Alle Schriftzeichen sind spiegelsymmetrisch.",
+        "Es handelt sich schlicht um chinesische Schriftzeichen unter einem anderen Namen."
+      ],
+      "Loesung": 0,
+      "Letter": "y"
+    },
+    {
+      "Frage": "Welche Superhelden verbergen sich hinter den schwedischen Namen \"Läderlappen\" und \"Spindelmannen\"?",
+      "Kategorie": "Länder",
+      "Antworten": [
+	"Batman und Spider-Man",
+	"Superman und Hulk",
+	"Catwoman und Iron Man"
+      ],
+      "Loesung": 0,
+      "Letter": "p"
+    },
+    {
+      "Frage": "Wie wahrscheinlich ist alle 15 Fragen bei antolina falsch zu beantworten?",
+      "Kategorie": "antolina",
+      "Antworten": [
+        "ca. 1:202",
+        "ca. 1:438",
+        "ca. 1:2381"
+      ],
+      "Letter": "p",
+      "Loesung": 1
+    },
+    {
+      "Frage": "Wer hat es geschafft, alle 15 Fragen bei antolina falsch zu beantworten?",
+      "Kategorie": "antolina",
+      "Antworten": [
+        "Franziska",
+        "Katharina",
+        "Beate"
+      ],
+      "Letter": "a",
+      "Loesung": 2
+    },
+    {
+      "Frage": "Was ist die Antwort auf die Frage nach dem Leben, dem Universum und dem ganzen Rest?",
+      "Kategorie": "Geburtstag",
+      "Antworten": [
+        "40",
+        "41",
+        "42"
+      ],
+      "Letter": "H",
+      "Loesung": 2
+    }
+  ]
+};
